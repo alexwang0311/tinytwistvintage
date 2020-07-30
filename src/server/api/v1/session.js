dd83ca8002cde738af7a8ebf7c51d8f3bc70dc9f
Zhixiang Wang <alexw@Zhixiangs-MacBook-Air.local>
2020-07-30 13:02:28 -0400

    Glitterdog101* pw

--- src/server/api/v1/session.js
+++ src/server/api/v1/session.js
@@ -1,7 +1,7 @@
 module.exports = app => {
     app.post("/v1/session", (req, res) => {
         const username = "c51613ba83cfe16815fb6d0a559ade20074076a638b4e6cee13dda3c372805a7";
-        const password = "19240b583023070a9216a3a08eeb2859b1a99ebe054ff7aae563e17bae9bc549";
+        const password = "f0f6f0099a18c32189697c8e7dab8ca1a19a54a2372f312810a401de31f4346b";
         if(req.body.username == username && req.body.password == password){
             res.status(200).send(JSON.stringify({success: "Success"}));
         }
